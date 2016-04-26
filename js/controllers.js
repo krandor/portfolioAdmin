e8b8bfa2b3dcc22d9044feef2e2dd001f1197019
Daniel Colomb <Daniel Colomb>
2016-04-25 22:06:42 -0400

    add some more test data to fill the page a bit more

--- js/controllers.js
+++ js/controllers.js
@@ -5,18 +5,20 @@ app.controller('DashboardController', function($scope) {
     name:"krandor",
     screenname:"krandor",
     avatarURI:"images/users/1_sm.jpg",
-    status: "Offline",
+    status: "Online",
     messages: [], comments: [],
     downloads: [{project:{name: "Highlander", description:"In-house-built CRM system"}, downloadCount:3},
                 {project:{name:"CyberXMPP", description:"In-office chat program."}, downloadCount:3},
-                {project:{name:"Eve-Online Mining Share Calculator", description:"Tool to calculate monitary distribution of mining profits for the MMORPG 'Eve-Online'"}, downloadCount:3}],
+                {project:{name:"Eve-Online Mining Share Calculator", description:"Tool to calculate monitary distribution of mining profits for the MMORPG 'Eve-Online'"}, downloadCount:3},
+                {project:{name:"Eve-Online Wormhole Ops", description:"Tool to calculate monitary distribution of wormhole operations for the MMORPG 'Eve-Online'."}, downloadCount: 4}],
     saves: 2,
     projects: [
       {name: "Highlander", description:"In-house-built CRM system"},
       {name:"CyberXMPP", description:"In-office chat program."},
       {name:"Eve-Online Mining Share Calculator", description:"Tool to calculate monitary distribution of mining profits for the MMORPG 'Eve-Online'"},
       {name:"Service Sentinel", description:"Service Suit to monitor Servers and Services"},
-      {name:"PortfolioAdmin", description:"Application to manage your professional portfolio."}
+      {name:"PortfolioAdmin", description:"Application to manage your professional portfolio."},
+      {name:"Eve-Online Wormhole Ops", description:"Tool to calculate monitary distribution of wormhole operations for the MMORPG 'Eve-Online'."}
     ]
   });
 
