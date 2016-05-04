aca7dab64bf1c998e41d0bc023f49e1f2eeffb38
Daniel Colomb <Daniel Colomb>
2016-05-04 18:59:25 -0400

    add testing libraries and configure tests

--- js/tests/user.tests.js
+++ js/tests/user.tests.js
@@ -4,4 +4,32 @@ describe('User', function(){
 
     expect(user).toBeDefined();
   });
+
+  it('should hold the name properly', function() {
+    var user = new User({name: "Gob", screenname: "GobTheMagnificent", status:"Online"});
+
+    expect(user).toBeDefined();
+    expect(user.name).toBe("Gob");
+  });
+
+  it('should hold the screenname properly', function() {
+    var user = new User({name: "Gob", screenname: "GobTheMagnificent", status:"Online"});
+
+    expect(user).toBeDefined();
+    expect(user.screenname).toBe("GobTheMagnificent");
+  });
+
+  it('should hold the status properly', function() {
+    var user = new User({name: "Gob", screenname: "GobTheMagnificent", status:"Online"});
+
+    expect(user).toBeDefined();
+    expect(user.status).toBe("Online");
+  });
+
+  it('should compute the statusCss properly', function() {
+    var user = new User({name: "Gob", screenname: "GobTheMagnificent", status:"Online"});
+
+    expect(user).toBeDefined();
+    expect(user.statusCss()).toBe("text-success");
+  });
 });
